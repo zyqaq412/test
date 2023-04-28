bc18fa79d486b1b94f1c17c500b30210757300df
zxwyhzy <2791517764@qq.com>
2023-04-28 15:23:08 +0800

    统一响应类+状态码枚举类

---
+++ 用户中心/userCenter/src/main/java/com/hzy/usercenter/enums/CodeEnum.java
@@ -0,0 +1,29 @@
+package com.hzy.usercenter.enums;
+
+/**
+ * @title: CodeEnum
+ * @Author zxwyhzy
+ * @Date: 2023/4/28 14:54
+ * @Version 1.0
+ */
+public enum CodeEnum {
+    // 成功
+    SUCCESS(200, "操作成功"),
+    // 失败
+    ERROR(400,"操作失败");
+    int code;
+    String msg;
+
+    CodeEnum(int code, String errorMessage) {
+        this.code = code;
+        this.msg = errorMessage;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
